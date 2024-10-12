f83d971bb249f20955fb09983d87827a1435515a
Adrian Alegarbes <60063188+heivia@users.noreply.github.com>
2024-10-12 23:37:11 +0800

    Update VisitorImgService.java

--- src/main/java/com/csit321/NaviGo/Service/VisitorImgService.java
+++ src/main/java/com/csit321/NaviGo/Service/VisitorImgService.java
@@ -1,22 +1,22 @@
 package com.csit321.NaviGo.Service;
-
+ 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+ 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+ 
 @Service
 public class VisitorImgService {
-
+ 
     @Value("${visitorimage.storage.path}")
     private String imageStoragePath;
-
+ 
     public String saveImage(MultipartFile file, String cardNo, String timeIn) throws IOException {
         Path storagePath = Paths.get(imageStoragePath);
         if (!Files.exists(storagePath)) {
@@ -27,11 +27,11 @@ public class VisitorImgService {
         Files.copy(file.getInputStream(), filePath);
         return filePath.toString();
     }
-
+ 
     public Resource loadImage(String cardNo, String timeIn) throws IOException {
         Path storagePath = Paths.get(imageStoragePath);
         String fileName = generateFileName(cardNo, timeIn);
-
+ 
         Path filePath = storagePath.resolve(fileName);
         if (Files.exists(filePath)) {
             return new UrlResource(filePath.toUri());
@@ -39,10 +39,10 @@ public class VisitorImgService {
             throw new IOException("File not found: " + fileName);
         }
     }
-
+ 
     private String generateFileName(String cardNo, String timeIn) {
     	  // Include date and time to avoid conflicts between AM/PM across days
     	  return "visitor_" + cardNo + "_" + timeIn + ".jpg";
     	}
-
+ 
 }
